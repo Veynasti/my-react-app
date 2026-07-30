05beaf2bcd1e4754252f9133992c8573a0de4eeb
vey <andreasdwiananda33@gmail.com>
2026-07-30 22:35:14 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import "./App.css";
+
+// function App() {
+//   return <div className="App"></div>
+// }
+
+// export default App;
+
+function App() {
+  return (
+    <div className="flex justify-center bg-blue-500 min-h-screen items-center">
+      <div>gini aja dulu</div>
+    </div>
+  );
+}
+
+export default App;
